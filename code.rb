944da8c2f810f48fcf1c7f346ffe4087e6b9827b
Blake Strickland <Stricklandb02@gmail.com>
2015-09-05 19:18:59 -0400

    Added symbol iscolate code. Added currency error file

--- code.rb
+++ code.rb
@@ -1,12 +1,17 @@
 #require './Converter.rb'
 require './currency.rb'
 
-money = Currency.new(100.0, "USD")
-money2 = Currency.new(10, "USD")
+money = Currency.new(100.0, :USD)
+money2 = Currency.new("€100")
 
-puts money * money2
+puts money.inspect
+puts money2.inspect
 
-puts money + money2
 
-puts money - money2
- 
+# puts money * money2
+#
+ puts money + money2
+#
+# puts money - money2
+#
+# puts money == money2
--- currency.rb
+++ currency.rb
@@ -1,35 +1,54 @@
+require './differentcurrencycodeerror.rb'
 class Currency
 
   attr_accessor :amount, :type
-  def initialize(amount, type)
-    @amount = amount
-    @type = type
-  end 
+  def initialize(amount, type = nil)
+    hash=Hash.new{}
+    hash= {
+      "$" => :USD,
+      "€" => :EUR,
+      "¥" => :YEN
+    }
+
+    if type
+      @amount = amount
+      @type = type.to_sym
+    else
+      @amount = amount[1..-1].to_f
+      @type = hash[amount[0]]
+
+
+    end
+  end
 
   def ==(equal)
     if @amount.==(equal.amount) && @type.==(equal.type)
-   else puts "DifferentCurrencyCodeError"
+   else
+     raise DifferentCurrencyCodeError
    end
   end
 
   def +(add)
     if @type == add.type
       @amount + add.amount
-    else puts "DifferentCurrencyCodeError"
+    else
+      raise DifferentCurrencyCodeError
     end
   end
 
   def -(subtract)
     if @type == subtract.type
       @amount - subtract.amount
-    else puts "DifferentCurrencyCodeError"
+    else
+      raise DifferentCurrencyCodeError
     end
   end
 
   def *(multiply)
     if @type == multiply.type
       @amount * multiply.amount
-    else puts "DifferentCurrencyCodeError"
+    else
+      raise DifferentCurrencyCodeError
     end
   end
  # money [1..-1]
