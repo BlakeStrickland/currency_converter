3a54b02925e7ec5bc9b52d78c207e47dc5ea5b96
Blake Strickland <Stricklandb02@gmail.com>
2015-09-07 18:37:52 -0400

    testing modifications

--- code.rb
+++ code.rb
@@ -2,16 +2,19 @@ require './Converter'
 require './currency'
 
 money = Currency.new("€89.89")
-money2 = Currency.new(100, :USD)
+money2 = Currency.new(10000, :USD)
 
 
 bank = Converter.new({USD: 1.00, EUR: 0.89894, YEN: 120.066})
 desired_type = :USD
+#money + money2
 money = bank.convert(money, desired_type)
 puts money.inspect
 money2 = bank.convert(money2, desired_type)
 money3 = money.amount + money2.amount
+money4 = money.amount * money2.amount
 
-puts money2.inspect
-puts money3.inspect
+money2.inspect
+puts money3
+puts money4
 #puts bank.convert(Currency.new(1, :USD), :USD) == Currency.new(1, :USD)
